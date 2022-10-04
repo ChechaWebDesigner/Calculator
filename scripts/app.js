1dfb22679284d9b249cff270a77fe51548ce4e78
César Joel González Oliva <113487509+ChechaWebDesigner@users.noreply.github.com>
2022-10-04 12:54:56 -0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -58,3 +58,47 @@ del.addEventListener("click", () => {
 calculate.addEventListener("click", () => {
   printScreen.operate();
 });
+
+window.addEventListener("keypress", (e) => {
+  validatePrintNumber(e.key);
+  validateSymbols(e.key);
+  if (e.key === "Enter") {
+    printScreen.operate();
+  }
+});
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const validatePrintNumber = (key) => {
+  const number = numbers.find((n) => n === parseFloat(key));
+
+  if (number === undefined || isNaN(number) || number === null) return;
+
+  if (initial === false) {
+    clear.initial();
+    printScreen.print(number);
+
+    initial = true;
+  } else {
+    printScreen.print(number);
+  }
+};
+
+const symbols = ["+", "-", "*", "/"];
+const validateSymbols = (key) => {
+  const symbol = symbols.find((sym) => sym === key);
+
+  if (symbol === undefined || symbol === null) return;
+
+  if (initial === false) {
+    printScreen.print(symbol);
+
+    initial = true;
+  } else {
+    if (printScreen.validationOperate()) {
+      printScreen.print(symbol);
+    } else {
+      console.log("hola");
+    }
+  }
+};
