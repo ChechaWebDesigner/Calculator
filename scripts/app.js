4d2702a084fbddf8ed38a237ee27fab20e318cd0
César Joel González Oliva <113487509+ChechaWebDesigner@users.noreply.github.com>
2022-10-04 17:38:58 -0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -83,6 +83,13 @@ window.addEventListener("keypress", (e) => {
   }
 });
 
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace") {
+    initialDef = clear.def();
+    initial = !initialDef;
+  }
+});
+
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const validatePrintNumber = (key) => {
